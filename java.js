6e8fc900aa6295934c69b005d3b89f5c779fc18d
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2024-01-25 00:56:02 +0000

    adding JS, api key

--- java.js
+++ java.js
@@ -4,3 +4,10 @@ console.log("Hello World");
 
 var searchForm = document.querySelector("#search-form");
 
+var apiKey = "d9e9a9f5f5a5b7a2c7c0b8f8b6e9d0b5";
+
+var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&" + "appid= + " + apiKey;
+
+var searchInput = document.querySelector("#search-input");
+
+https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
