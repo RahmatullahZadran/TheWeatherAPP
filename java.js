a0e461e66db391b5cee44956eb0cd7e8889626ad
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2024-01-25 09:08:31 +0000

    added JS for the header

--- java.js
+++ java.js
@@ -4,10 +4,47 @@ console.log("Hello World");
 
 var searchForm = document.querySelector("#search-form");
 
-var apiKey = "d9e9a9f5f5a5b7a2c7c0b8f8b6e9d0b5";
+var apiKey = "766bf34a17420469977c1a427d2a44ae";
 
-var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&" + "appid= + " + apiKey;
+var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ searchInput + "&" + "appid= + "+ apiKey;
 
 var searchInput = document.querySelector("#search-input");
 
-https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+$("#search-button").on("click", function (event) {
+    event.preventDefault();
+    console.log(searchInput.value);
+
+   //if emoty dont do anything
+    if (searchInput.value === "") {
+        return;
+    }
+
+    $("#history").append("<button class='btn btn-secondary history-button'>" + searchInput.value + "</button> ");
+    searchInput.value = "";
+
+    var buttonCount = $(".history-button").length;
+
+
+    // Limit the number of buttons to 6
+    if (buttonCount >= 6) {
+        // Remove the oldest button
+        $(".history-button:first").remove();
+    }
+
+})
+
+console.log(queryUrl);
+//forcast
+$('#today').append("<h1>Today's Forecast</h1>");
+
+//temp  
+$("#today").append("<p>Temp:</p>");
+
+//wind 
+$("#today").append("<p>Wind:</p>");
+
+//humidity
+$("#today").append("<p>Humidity:</p>");
+
+
+
